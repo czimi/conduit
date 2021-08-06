ad7e44640b218a402caa11b441021b3e1b73078d
czimi <czimi@hotmail.com>
2021-08-06 10:05:26 +0200

    Registration of a new user (precondition) - ver. 1.0.

--- test_conduit/test_TC4_signin.py
+++ test_conduit/test_TC4_signin.py
@@ -3,22 +3,23 @@ import allure
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
-
+from conduit_registration import *
 
 class TestConduitApp(object):
 
     def setup(self):
         browser_options = Options()
-        browser_options.headless = True
+        browser_options.headless = False
         self.browser = webdriver.Chrome(ChromeDriverManager().install(), options=browser_options)
         self.browser.get("http://localhost:1667/")
 
     def teardown(self):
         self.browser.quit()
 
-    # TC4 Sign in with an existing user
+    # TC4 Sign in (precondition: registration of a new user)
 
     def test_sign_in(self):
+        registration_user(self.browser)
         self.browser.find_element_by_xpath('//*[@id="app"]/nav/div/ul/li[2]/a').click()
         sign_in_email_input = self.browser.find_element_by_xpath(
             '//*[@id="app"]/div/div/div/div/form/fieldset[1]/input')
