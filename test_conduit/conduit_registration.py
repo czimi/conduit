c280cb6f1b6c1358fb05ff2da59b143207eaa173
czimi <czimi@hotmail.com>
2021-08-06 10:05:54 +0200

    Registration of a new user (precondition) - ver. 1.0.

---
+++ test_conduit/conduit_registration.py
@@ -0,0 +1,12 @@
+# Precondition of all TC-s:
+
+def registration_user(browser):
+    browser.find_element_by_xpath('//*[@id="app"]/nav/div/ul/li[3]/a').click()
+    username_input = browser.find_element_by_xpath('//*[@id="app"]/div/div/div/div/form/fieldset[1]/input')
+    email_input = browser.find_element_by_xpath('//*[@id="app"]/div/div/div/div/form/fieldset[2]/input')
+    password_input = browser.find_element_by_xpath('//*[@id="app"]/div/div/div/div/form/fieldset[3]/input')
+    signup_btn = browser.find_element_by_xpath('//*[@id="app"]/div/div/div/div/form/button')
+    username_input.send_keys("Próba Pista 0")
+    email_input.send_keys("proba_pista_0@proba.com")
+    password_input.send_keys("Proba123")
+    signup_btn.click()
