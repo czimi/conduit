f00dd06d1d0d193da67dccfaf6496a4508edaaf2
czimi <czimi@hotmail.com>
2021-08-06 19:52:32 +0200

    TC1(accept_cookies) ver. 1.0

---
+++ test_conduit/test_TC1_accept_cookies.py
@@ -0,0 +1,26 @@
+import time
+import allure
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+class TestConduitApp(object):
+
+    def setup(self):
+        browser_options = Options()
+        browser_options.headless = False
+        self.browser = webdriver.Chrome(ChromeDriverManager().install(), options=browser_options)
+        self.browser.get("http://localhost:1667/")
+
+    def teardown(self):
+        self.browser.quit()
+
+# TC1 accept cookies
+    @allure.severity(allure.severity_level.NORMAL)
+    def test_accept_cookies(self):
+        time.sleep(2)
+        self.browser.find_element_by_xpath('//button[contains(@class, "accept")]').click()
+        time.sleep(2)
+
+        assert self.browser.get_cookies()[0]['value'] == 'accept'
