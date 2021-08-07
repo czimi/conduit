7156f8aebdec6236658476ce0ecb2005c278fe3f
czimi <czimi@hotmail.com>
2021-08-08 00:47:10 +0200

    Registration of a new user (precondition) - ver. 1.3. Reformat all xpath.

--- test_conduit/conduit_registration.py
+++ test_conduit/conduit_registration.py
@@ -1,17 +1,19 @@
-# Precondition of all TC-s:
+# Precondition of all TCs':
 import time
 
 
 def registration_user(browser):
-    browser.find_element_by_xpath('//*[@id="app"]/nav/div/ul/li[3]/a').click()
-    username_input = browser.find_element_by_xpath('//*[@id="app"]/div/div/div/div/form/fieldset[1]/input')
-    email_input = browser.find_element_by_xpath('//*[@id="app"]/div/div/div/div/form/fieldset[2]/input')
-    password_input = browser.find_element_by_xpath('//*[@id="app"]/div/div/div/div/form/fieldset[3]/input')
-    signup_btn = browser.find_element_by_xpath('//*[@id="app"]/div/div/div/div/form/button')
+    browser.find_element_by_xpath('//a[normalize-space()="Sign up"]').click()
+    username_input = browser.find_element_by_xpath('//input[@placeholder="Username"]')
+    email_input = browser.find_element_by_xpath('//input[@placeholder="Email"]')
+    password_input = browser.find_element_by_xpath('//input[@placeholder="Password"]')
+    signup_btn = browser.find_element_by_xpath(' //button[normalize-space()="Sign up"]')
+
     username_input.send_keys("Próba Pista 0")
     email_input.send_keys("proba_pista_0@proba.com")
     password_input.send_keys("Proba123")
     signup_btn.click()
     time.sleep(2)
-    confirm_btn = browser.find_element_by_xpath('//button[contains(.,"OK")]')
+    confirm_btn = browser.find_element_by_xpath('//button[normalize-space()="OK"]')
     confirm_btn.click()
+    time.sleep(2)
