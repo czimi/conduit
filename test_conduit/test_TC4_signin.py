98cca22295bf3280bb799a30c4ce2426469b8959
czimi <czimi@hotmail.com>
2021-08-07 20:23:55 +0200

    TC4(sign_in) ver. 1.3. Reformat xpath expressions.

--- test_conduit/test_TC4_signin.py
+++ test_conduit/test_TC4_signin.py
@@ -20,17 +20,15 @@ class TestConduitApp(object):
 
     def test_sign_in(self):
         registration_user(self.browser)
-        self.browser.find_element_by_xpath('//*[@id="app"]/nav/div/ul/li[2]/a').click()
-        sign_in_email_input = self.browser.find_element_by_xpath(
-            '//*[@id="app"]/div/div/div/div/form/fieldset[1]/input')
-        sign_in_password_input = self.browser.find_element_by_xpath(
-            '//*[@id="app"]/div/div/div/div/form/fieldset[2]/input')
-        sign_in_btn = self.browser.find_element_by_xpath('//*[@id="app"]/div/div/div/div/form/button')
+        self.browser.find_element_by_xpath('//a[contains(.,"Sign in)]').click()
+        sign_in_email_input = self.browser.find_element_by_xpath('//input[@placeholder="Email"]')
+        sign_in_password_input = self.browser.find_element_by_xpath('//input[@placeholder="Password"]')
+        sign_in_btn = self.browser.find_element_by_xpath('//button[contains(.,"Sign in")]')
 
         sign_in_email_input.send_keys("proba_pista_0@proba.com")
         sign_in_password_input.send_keys("Proba123")
         sign_in_btn.click()
         time.sleep(2)
 
-        username_link = self.browser.find_element_by_xpath('//*[@id="app"]/nav/div/ul/li[4]/a')
+        username_link = self.browser.find_element_by_xpath('//a[@class="nav-link"][normalize-space()="Próba Pista 0"]')
         assert username_link.text == 'Próba Pista 0'
