33ea19b9daf2b40975bb43865f77ba79ab2b04d4
czimi <czimi@hotmail.com>
2021-08-08 11:34:20 +0200

    TC3(sign_up_dynamic) ver. 1.2. Fixed user_variable reference issue.

--- test_conduit/test_TC3_signup_dynamic.py
+++ test_conduit/test_TC3_signup_dynamic.py
@@ -8,8 +8,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-user_variable = random.randint(1, 100)
-
 
 class TestConduitApp(object):
 
@@ -18,21 +16,22 @@ class TestConduitApp(object):
         browser_options.headless = True
         self.browser = webdriver.Chrome(ChromeDriverManager().install(), options=browser_options)
         self.browser.get("http://localhost:1667/")
+        self.user_variable = random.randint(1, 100)
 
     def teardown(self):
         self.browser.quit()
 
-    # TC3 Sign up with an appropriate password and "dinamic" user datas
+    # TC3 Sign up with an appropriate password and "dynamic" user data
 
-    def test_registration_dinamic_userdata(self):
+    def test_registration_dynamic_userdata(self):
         self.browser.find_element_by_xpath('//a[normalize-space()="Sign up"]').click()
         username_input = self.browser.find_element_by_xpath('//input[@placeholder="Username"]')
         email_input = self.browser.find_element_by_xpath('//input[@placeholder="Email"]')
         password_input = self.browser.find_element_by_xpath('//input[@placeholder="Password"]')
         signup_btn = self.browser.find_element_by_xpath('//button[normalize-space()="Sign up"]')
 
-        username_input.send_keys(f"Próba Pista {user_variable}")
-        email_input.send_keys(f"proba_pista_{user_variable}@proba.com")
+        username_input.send_keys(f"Próba Pista {self.user_variable}")
+        email_input.send_keys(f"proba_pista_{self.user_variable}@proba.com")
         password_input.send_keys("Proba123")
         signup_btn.click()
 
