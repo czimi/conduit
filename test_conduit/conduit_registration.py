c4d40d9670073c1f6b4affb365181fbd50e8ae75
czimi <czimi@hotmail.com>
2021-08-09 10:48:56 +0200

    Registration of a new user and Sign in (precondition) - ver. 2.1.

--- test_conduit/conduit_registration.py
+++ test_conduit/conduit_registration.py
@@ -4,7 +4,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-def registration_user(browser):
+def registration_and_login_user(browser):
     browser.find_element_by_xpath('//a[normalize-space()="Sign up"]').click()
     username_input = browser.find_element_by_xpath('//input[@placeholder="Username"]')
     email_input = browser.find_element_by_xpath('//input[@placeholder="Email"]')
@@ -12,7 +12,7 @@ def registration_user(browser):
     signup_btn = browser.find_element_by_xpath('//button[normalize-space()="Sign up"]')
 
     username_input.send_keys("Próba Pista 0")
-    email_input.send_keys("proba_pista_0@proba.com")
+    email_input.send_keys("proba_pista_0Y@proba.com")
     password_input.send_keys("Proba123")
     signup_btn.click()
 
@@ -22,3 +22,21 @@ def registration_user(browser):
     )
     element.click()
 
+    username_link = browser.find_element_by_xpath('//a[contains(@href,"#/@")]')
+    if username_link.is_displayed():
+        return
+    else:
+        # def login_user(browser):
+        element = WebDriverWait(
+            browser, 10).until(
+            EC.visibility_of_element_located((By.XPATH, '//a[@href="#/login"]'))
+        )
+        element.click()
+
+        sign_in_email_input = browser.find_element_by_xpath('//input[@placeholder="Email"]')
+        sign_in_password_input = browser.find_element_by_xpath('//input[@placeholder="Password"]')
+        sign_in_btn = browser.find_element_by_xpath('//button[contains(.,"Sign in")]')
+
+        sign_in_email_input.send_keys("proba_pista_0Y@proba.com")
+        sign_in_password_input.send_keys("Proba123")
+        sign_in_btn.click()
