7055ca1ecddf5ffeb6c04cea0bf64df8d36794af
czimi <czimi@hotmail.com>
2021-08-10 14:23:05 +0200

    Registration of a new user and Sign in (precondition) - ver. 2.2. Set user email to proba_pista_o@proba.com.

--- test_conduit/conduit_registration.py
+++ test_conduit/conduit_registration.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+email_elotag = "proba_pista+0D"
 
 def registration_and_login_user(browser):
     browser.find_element_by_xpath('//a[normalize-space()="Sign up"]').click()
@@ -11,8 +12,8 @@ def registration_and_login_user(browser):
     password_input = browser.find_element_by_xpath('//input[@placeholder="Password"]')
     signup_btn = browser.find_element_by_xpath('//button[normalize-space()="Sign up"]')
 
-    username_input.send_keys("Próba Pista 0")
-    email_input.send_keys("proba_pista_0@proba.com")
+    username_input.send_keys("Próba Pista 0C")
+    email_input.send_keys(f"{email_elotag}@proba.com")
     password_input.send_keys("Proba123")
     signup_btn.click()
 
@@ -22,21 +23,24 @@ def registration_and_login_user(browser):
     )
     element.click()
 
-    username_link = browser.find_element_by_xpath('//a[contains(@href,"#/@")]')
-    if username_link.is_displayed():
-        return
-    else:
-        # def login_user(browser):
-        element = WebDriverWait(
-            browser, 10).until(
-            EC.visibility_of_element_located((By.XPATH, '//a[@href="#/login"]'))
-        )
-        element.click()
+def conduit_logout(browser):
+    browser.find_element_by_xpath('//a[@active-class="active"]').click()
 
-        sign_in_email_input = browser.find_element_by_xpath('//input[@placeholder="Email"]')
-        sign_in_password_input = browser.find_element_by_xpath('//input[@placeholder="Password"]')
-        sign_in_btn = browser.find_element_by_xpath('//button[contains(.,"Sign in")]')
+    # username_link = browser.find_elements_by_xpath('//a[contains(@href,"#/@")]')
+    # # if username_link.is_displayed():
+    # #     return None
+    # if len(username_link) == 0:
+    #     element = WebDriverWait(
+    #         browser, 10).until(
+    #         EC.visibility_of_element_located((By.XPATH, '//a[@href="#/login"]'))
+    #     )
+    #     element.click()
+    #
+    #     sign_in_email_input = browser.find_element_by_xpath('//input[@placeholder="Email"]')
+    #     sign_in_password_input = browser.find_element_by_xpath('//input[@placeholder="Password"]')
+    #     sign_in_btn = browser.find_element_by_xpath('//button[contains(.,"Sign in")]')
+    #
+    #     sign_in_email_input.send_keys("proba_pista_0Y@proba.com")
+    #     sign_in_password_input.send_keys("Proba123")
+    #     sign_in_btn.click()
 
-        sign_in_email_input.send_keys("proba_pista_0@proba.com")
-        sign_in_password_input.send_keys("Proba123")
-        sign_in_btn.click()
--- test_conduit/test_TC4_signin.py
+++ test_conduit/test_TC4_signin.py
@@ -9,7 +9,7 @@ class TestConduitApp(object):
 
     def setup(self):
         browser_options = Options()
-        browser_options.headless = True
+        browser_options.headless = False
         self.browser = webdriver.Chrome(ChromeDriverManager().install(), options=browser_options)
         self.browser.get("http://localhost:1667/")
 
@@ -20,6 +20,7 @@ class TestConduitApp(object):
 
     def test_sign_in(self):
         registration_and_login_user(self.browser)
+        conduit_logout(self.browser)
 
         WebDriverWait(
             self.browser, 10).until(
@@ -30,10 +31,10 @@ class TestConduitApp(object):
         sign_in_password_input = self.browser.find_element_by_xpath('//input[@placeholder="Password"]')
         sign_in_btn = self.browser.find_element_by_xpath('//button[contains(.,"Sign in")]')
 
-        sign_in_email_input.send_keys("proba_pista_0@proba.com")
+        sign_in_email_input.send_keys(f"{email_elotag}@proba.com")
         sign_in_password_input.send_keys("Proba123")
         sign_in_btn.click()
         time.sleep(2)
 
-        username_link = self.browser.find_element_by_xpath('//a[@class="nav-link"][normalize-space()="Próba Pista 0"]')
-        assert username_link.text == 'Próba Pista 0'
+        username_link = self.browser.find_element_by_xpath('//a[@class="nav-link"][normalize-space()="Próba Pista 0C"]')
+        assert username_link.text == 'Próba Pista 0C'
