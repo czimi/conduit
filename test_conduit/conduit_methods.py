f86e809eb8987923f8f99feda39fc000982aee35
czimi <czimi@hotmail.com>
2021-08-10 16:46:08 +0200

    Registration of a new user (precondition) - ver. 3.1. Without if-statement.

--- test_conduit/conduit_methods.py
+++ test_conduit/conduit_methods.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-username_variable = "0D"
+username_variable = "0E"
 email_elotag = f"proba_pista_{username_variable}"
 
 def conduit_registration(browser):
@@ -27,21 +27,20 @@ def conduit_registration(browser):
 def conduit_logout(browser):
     browser.find_element_by_xpath('//a[@active-class="active"]').click()
 
-    username_link = browser.find_elements_by_xpath('//a[contains(@href,"#/@")]')
-    # if username_link.is_displayed():
-    #     return None
-    if len(username_link) == 0:
-        element = WebDriverWait(
-            browser, 10).until(
-            EC.visibility_of_element_located((By.XPATH, '//a[@href="#/login"]'))
-        )
-        element.click()
+    # username_link = browser.find_elements_by_xpath('//a[contains(@href,"#/@")]')
 
-        sign_in_email_input = browser.find_element_by_xpath('//input[@placeholder="Email"]')
-        sign_in_password_input = browser.find_element_by_xpath('//input[@placeholder="Password"]')
-        sign_in_btn = browser.find_element_by_xpath('//button[contains(.,"Sign in")]')
-
-        sign_in_email_input.send_keys(f"proba_pista_{email_elotag}@proba.com")
-        sign_in_password_input.send_keys("Proba123")
-        sign_in_btn.click()
+    # if len(username_link) == 0:
+    #     element = WebDriverWait(
+    #         browser, 10).until(
+    #         EC.visibility_of_element_located((By.XPATH, '//a[@href="#/login"]'))
+    #     )
+    #     element.click()
+    #
+    #     sign_in_email_input = browser.find_element_by_xpath('//input[@placeholder="Email"]')
+    #     sign_in_password_input = browser.find_element_by_xpath('//input[@placeholder="Password"]')
+    #     sign_in_btn = browser.find_element_by_xpath('//button[contains(.,"Sign in")]')
+    #
+    #     sign_in_email_input.send_keys(f"proba_pista_{email_elotag}@proba.com")
+    #     sign_in_password_input.send_keys("Proba123")
+    #     sign_in_btn.click()
 
