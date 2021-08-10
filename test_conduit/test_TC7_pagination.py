cb6bbccc9f7dbf49d4f3404a2e76d977fe4b27d5
czimi <czimi@hotmail.com>
2021-08-10 09:10:50 +0200

    TC7(pagination) ver. 1.1. The def registration_and_login_user renamed.

--- test_conduit/test_TC7_pagination.py
+++ test_conduit/test_TC7_pagination.py
@@ -18,7 +18,7 @@ class TestConduitApp(object):
 
     # TC7 Pagination on Conduit (precondition: registration of a new user)
     def test_pagination(self):
-        registration_user(self.browser)
+        registration_and_login_user(self.browser)
         time.sleep(2)
         page_1 = self.browser.find_element_by_xpath('//li[@data-test="page-link-1"]')
         assert page_1.get_attribute("class") == "page-item active"
