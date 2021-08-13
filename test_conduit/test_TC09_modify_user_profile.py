4560fd009455ba97498421f79d996aeb7a9db85b
czimi <czimi@hotmail.com>
2021-08-14 01:29:10 +0200

    TC09(modify_user_profile) - ver. 1.1. time.sleep

--- test_conduit/test_TC09_modify_user_profile.py
+++ test_conduit/test_TC09_modify_user_profile.py
@@ -1,4 +1,4 @@
-
+import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
@@ -6,6 +6,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from conduit_methods import *
+
 
 class TestConduitApp(object):
 
@@ -18,12 +19,14 @@ class TestConduitApp(object):
     def teardown(self):
         self.browser.quit()
 
-# TC09 Modify user_profile (precondition: sign in)
+    # TC09 Modify user_profile (precondition: sign in)
     def test_modify_user_name(self):
         conduit_login(self.browser)
+        WebDriverWait(
+            self.browser, 10).until(
+            EC.visibility_of_element_located((By.XPATH, '//a[@href="#/settings"]'))
+        ).click()
         time.sleep(3)
-        self.browser.find_element_by_xpath('//a[@href="#/settings"]').click()
-        time.sleep(2)
         input_your_username = self.browser.find_element_by_xpath('//input[@placeholder="Your username"]')
         input_your_username.clear()
         input_your_username.send_keys(f"Próba Pista {username_variable}C")
@@ -33,7 +36,7 @@ class TestConduitApp(object):
             EC.visibility_of_element_located((By.XPATH, '//button[normalize-space()="OK"]'))
         )
         update_confirm_btn.click()
-        new_username_link = self.browser.find_element_by_xpath(f'//a[@class="nav-link"][contains(.,"Próba Pista {username_variable}C")]')
+        new_username_link = self.browser.find_element_by_xpath(
+            f'//a[@class="nav-link"][contains(.,"Próba Pista {username_variable}C")]')
         assert new_username_link.is_displayed()
         username_reset(self.browser)
-
