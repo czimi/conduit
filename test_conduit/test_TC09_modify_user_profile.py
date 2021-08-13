7a9a034025dda050d3b9e049837add0853cd271f
czimi <czimi@hotmail.com>
2021-08-13 14:54:15 +0200

    TC09(modify_user_profile) - ver. 1.0.

--- test_conduit/test_TC09_modify_user_profile.py
+++ test_conduit/test_TC09_modify_user_profile.py
@@ -1,6 +1,4 @@
-import time
-import allure
-from allure_commons.types import AttachmentType
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
@@ -20,19 +18,20 @@ class TestConduitApp(object):
     def teardown(self):
         self.browser.quit()
 
-# TC09 Modify blogpost content (precondition: sign in)
-    def modify_blogpost_content(self):
+# TC09 Modify user_profile (precondition: sign in)
+    def test_modify_user_name(self):
         conduit_login(self.browser)
-        create_blogpost_for_testing(self.browser)
         time.sleep(3)
-        self.browser.find_element_by_xpath('//a[contains(normalize-space(),"Home")]').click()
+        self.browser.find_element_by_xpath('//a[@href="#/settings"]').click()
         time.sleep(2)
-        self.browser.find_element_by_xpath(f'//h1[contains(.,"test_blogpost_{datetime_now}")]').click()
-        time.sleep(2)
-        self.browser.find_element_by_xpath('//a[contains(.,"Edit Article")]').click()
-        time.sleep(2)
-        blogpost_text_input = self.browser.find_element_by_xpath('//textarea[@placeholder="Write your article (in markdown)"]')
-        blogpost_text_input.clear()
-        blogpost_text_input.send_keys('A kasárában egy eléggé piszalmarsodt, önözös tratokra nem hegyezdő, fürk várkódt ki. Valaki azért, mert neki nem lesztett meg a hált srót és gyorica, mások pedig saját párdáruk bosztatását kélkedik be.')
-        self.browser.find_element_by_xpath('//button[normalize-space()="Publish Article"]').click()
-
+        input_your_username = self.browser.find_element_by_xpath('//input[@placeholder="Your username"]')
+        input_your_username.clear()
+        input_your_username.send_keys(f"Próba Pista {username_variable}C")
+        self.browser.find_element_by_xpath('//button[normalize-space()="Update Settings"]').click()
+        update_confirm_btn = WebDriverWait(
+            self.browser, 10).until(
+            EC.visibility_of_element_located((By.XPATH, '//button[normalize-space()="OK"]'))
+        )
+        update_confirm_btn.click()
+        new_username_link = self.browser.find_element_by_xpath(f'//a[@class="nav-link"][contains(.,"Próba Pista {username_variable}C")]')
+        assert new_username_link.is_displayed()
